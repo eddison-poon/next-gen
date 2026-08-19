5e2eb8829bdaed81b3efcdc75d4bf5ff5e64ca5c
eddison-poon <namidae@gmail.com>
2026-08-19 17:06:21 +0800

    v0.8 include operator workflow tests in UAT checks

--- tools/run_uat_checks.py
+++ tools/run_uat_checks.py
@@ -21,6 +21,7 @@ run([sys.executable,"tools/validate_uat_candidate.py"])
 tests=[]
 tests += load_tests(ROOT/"tests/test_release_reporting.py","release_reporting_tests")
 tests += load_tests(ROOT/"tests/test_data_onboarding.py","data_onboarding_tests")
+tests += load_tests(ROOT/"tests/test_uat_operator_workflow.py","uat_operator_workflow_tests")
 
 for test in tests:
     test()
